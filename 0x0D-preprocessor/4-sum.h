d32bd3c1077431ae7fe855db59913785ee094145
newtef12 <newtef398@gmail.com>
2023-08-25 14:49:23 -0700

    updated

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,6 @@
 #ifndef SUM_H
 #define SUM_H
 
-/**
- * SUM - Computes the sum of two numbers x and y.
- * @x: The first input number.
- * @y: The second input number.
- *
- * Return: Sum of x and y.
- */
 #define SUM(x, y) ((x) + (y))
 
 #endif
