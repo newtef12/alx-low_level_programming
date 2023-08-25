26a59b0b4078b04cbaf1d5e8e9b19ea7dda39622
newtef12 <newtef398@gmail.com>
2023-08-25 14:43:41 -0700

    updated

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/**
- * ABS - Computes the absolute value of a number x.
- * @x: The input number.
- *
- * Return: Absolute value of x.
- */
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
