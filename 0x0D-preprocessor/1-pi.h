1321ced61dcd3690b231e58c1cc5b78ca5389835
newtef12 <newtef398@gmail.com>
2023-08-25 14:26:27 -0700

    upate

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef MY_PI_H
+#define MY_PI_H
+
+/* Define a macro named MY_PI as an abbreviation for the token 3.14159265359 */
+#define MY_PI 3.14159265359
+
+#endif /* MY_PI_H */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number x.
+ * @x: The input number.
+ *
+ * Return: Absolute value of x.
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
