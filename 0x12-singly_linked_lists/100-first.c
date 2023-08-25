269b92b77a3e95165797f29d67b94426b41b3a8e
newtef12 <newtef398@gmail.com>
2023-08-24 17:35:55 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void execute_before_main(void) __attribute__ ((constructor));
+void before_main(void) __attribute__ ((constructor));
 
 /**
- * execute_before_main - Prints a message before the main function is executed.
+ * before_main - Prints a message before the main function is executed.
  */
-void execute_before_main(void)
+void before_main(void)
 {
-    printf("You're here early! But let me tell you,\n");
-    printf("I carry my home on my back!\n");
+    printf("You're conquered! and still, you must acknowledge,\n");
+    printf("I carry my dwelling on my dorsal ridge!\n");
 }
