ad890f73e1bf5b5e79d3192a138c81426a557f61
newtef12 <newtef398@gmail.com>
2023-08-31 13:42:13 -0700

    updated

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A string representing the binary number.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: The converted number, or 0 if invalid input.
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-    if (!b)
-        return 0;
+	if (!b)
+		return (0);
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] < '0' || b[i] > '1')
-            return 0;
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-        result = (result * 2) + (b[i] - '0');
-    }
-
-    return result;
+	return (dec_val);
 }
