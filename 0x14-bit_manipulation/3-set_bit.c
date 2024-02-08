3b2da38f12ffa457068eca4b5c73b55575833fa2
newtef12 <newtef398@gmail.com>
2024-02-08 11:25:29 -0800

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * set_bit - prog sets the value of a bit to 1 at a given index.
- * @n: number to set
- * @index: index at which to set bit
- *
- * Return: 1 if success, or -1 if an error occurred
+ * set_bit - the given index sets a bit 1
+ * @n: it's nothing but a pointer to change a number
+ * @index: An index that set a bit 1
+ * Return: 1  success, -1 fail
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int setbit;
-
-	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return (-1);
-	setbit = 1 << index;
-	*n = *n | setbit;
-	return (1);
+        if (index > 63)
+                return (-1);
+        *n = ((1UL << index) | *n);
+        return (1);
 }
