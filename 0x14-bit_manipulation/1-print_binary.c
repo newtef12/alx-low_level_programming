455bd5a24f3ab52f81d01de7e3f1cb002d3b202c
newtef12 <newtef398@gmail.com>
2024-02-08 11:18:45 -0800

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,48 +1,26 @@
 #include "main.h"
 
 /**
- * _pow - func calculates (base ^ power)
- * @base: base of the exponent
- * @power: power of the exponent
- *
- * Return: value of (base ^ power)
+ * print_binary - equivalent of a decimal number that prints a binary
+ * @n: the amount or number binary to be printed
  */
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-	unsigned long int num;
-	unsigned int a;
 
-	num = 1;
-	for (a = 1; a <= power; a++)
-		num *= base;
-	return (num);
-}
-
-/**
- * print_binary - prints a number in binary notation
- * @n: number to print
- *
- * Return: void
- */
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor, check;
-	char flag;
+        int leona, Sis_hoffmann = 0;
+        unsigned long int new;
 
-	flag = 0;
-	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
-	while (divisor != 0)
-	{
-		check = n & divisor;
-		if (check == divisor)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else if (flag == 1 || divisor == 1)
-		{
-			_putchar('0');
-		}
-		divisor >>= 1;
-	}
+        for (leona = 63; leona >= 0; leona--)
+        {
+                new = n >> leona;
+                if (new & 1)
+                {
+                        _putchar('1');
+                        Sis_hoffmann++;
+                }
+                else if (new)
+                        _putchar('0');
+        }
+        if (!new)
+                _putchar('0');
 }
