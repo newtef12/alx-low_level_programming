aba081aff41e3a9db38aab01ad099697313d13fc
newtef12 <newtef398@gmail.com>
2024-03-21 13:40:08 -0700

    nn

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,39 +1,5 @@
 #include <stdlib.h>
-
-/* Define the dlistint_t structure */
-struct dlistint_s
-{
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-};
-typedef struct dlistint_s dlistint_t;
-
-/**
- * dlistint_len - Returns the number of elements in a linked dlistint_t list.
- * @h: Pointer to the head of the list
- * Return: The number of elements in the list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-    size_t count = 0;
-    const dlistint_t *temp = h;
-
-    /* Traverse to find the actual head */
-    while (temp && temp->prev != NULL)
-        temp = temp->prev;
-
-    /* Now 'temp' points to the actual head of the list */
-
-    /* Traverse the list and count its elements */
-    while (temp != NULL)
-    {
-        count++;
-        temp = temp->next;
-    }
-
-    return count;
-}
+#include "lists.h"
 
 /**
  * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
