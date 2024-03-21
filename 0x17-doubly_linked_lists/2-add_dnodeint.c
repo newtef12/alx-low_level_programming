bc5836d50fcf1f51f67301e4bffae58a2a780f20
newtef12 <newtef398@gmail.com>
2024-03-21 13:43:29 -0700

    nn

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
     dlistint_t *new_node;
 
+    if (head == NULL)
+        return NULL;
+
     /* Initialize the new node */
     new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
